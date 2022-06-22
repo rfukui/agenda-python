d688775a493f47316937c80b369ae2c311523198
rFukui <ricardo@clubbi.com.br>
2022-06-22 17:25:01 -0300

    adcionado testes para funcao e classe

--- professores/services.py
+++ professores/services.py
@@ -1,8 +1,10 @@
 from .models import Professor
 
 
-def cria_professor(professor: Professor) -> Professor:
-    ...
+def cria_professor(professor: dict) -> Professor:
+    new_professor = Professor(professor)
+    new_professor.save()
+    return new_professor
 
 def consulta_professor(dicionario:dict) -> Professor:
     ...
--- test/professores/tests_services.py
+++ test/professores/tests_services.py
@@ -1,11 +1,16 @@
 import pytest
 from unittest.mock import MagicMock, patch
 from professores import services
-from professores.models import Professor
+
+
 class TestProfessorServices:
 
     @patch("professores.services.Professor")
     def test_create_professor(self, professor_mock):
-        profe = Professor()
-        new_professor = services.cria_professor(profe)
-        professor_mock.save.has_been_called()
+        professor_mock.save = MagicMock()
+        profe = {}
+
+        services.cria_professor(profe)
+
+        professor_mock.assert_called_once_with({})
+        professor_mock().save.assert_called_once()
