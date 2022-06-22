486286477a9295ffae17e2219382851b1a54aa28
rFukui <ricardo@clubbi.com.br>
2022-06-22 18:40:05 -0300

    add tests to side_effect error, not working

--- professores/services.py
+++ professores/services.py
@@ -3,7 +3,12 @@ from .models import Professor
 
 def cria_professor(professor: dict) -> Professor:
     new_professor = Professor(professor)
-    new_professor.save()
+    if professor["name"] == "teste":
+        return new_professor
+    try:
+        new_professor.save()
+    except Exception:
+        return None
     return new_professor
 
 def consulta_professor(dicionario:dict) -> Professor:
--- test/professores/tests_services.py
+++ test/professores/tests_services.py
@@ -1,16 +1,38 @@
 import pytest
 from unittest.mock import MagicMock, patch
 from professores import services
+from professores.models import Professor
 
 
 class TestProfessorServices:
 
     @patch("professores.services.Professor")
     def test_create_professor(self, professor_mock):
-        professor_mock.save = MagicMock()
-        profe = {}
+        professor_mock.save = MagicMock(return_value=Professor)
+        profe = {"name": "pedro", }
 
         services.cria_professor(profe)
 
-        professor_mock.assert_called_once_with({})
+        professor_mock.assert_called_once_with(profe)
         professor_mock().save.assert_called_once()
+
+    @patch("professores.services.Professor")
+    def test_create_teste_professor(self, professor_mock):
+        professor_mock.save = MagicMock(return_value=Professor)
+        profe = {"name": "teste", }
+
+        services.cria_professor(profe)
+
+        professor_mock.assert_called_once_with(profe)
+        professor_mock().save.assert_not_called()
+
+    @patch("professores.services.Professor")
+    def test_error(self, professor_mock):
+        professor_mock.save.side_effect = KeyError('foo')
+        profe = {"name": "Pedro", }
+
+        new_professor = services.cria_professor(profe)
+
+        professor_mock.assert_called_once_with(profe)
+        professor_mock().save.assert_called_once()
+        assert new_professor is None
